e84ad42e3e6f425b3ebe233167b42c2fa8d00144
Nue-ux <lenindanielsantacruzrodrigez@hotmail.com>
2024-12-02 17:22:12 +0100

    Todo funciona falta arreglar

--- app/src/main/java/com/example/android/FirstFragment.kt
+++ app/src/main/java/com/example/android/FirstFragment.kt
@@ -1,14 +1,17 @@
 package com.example.android
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.findNavController
 import com.example.android.databinding.FragmentFirstBinding
+import java.time.LocalDate
+import java.time.Period
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,11 +34,36 @@ class FirstFragment : Fragment() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Apply a color filter to the ImageButton
         binding.bottom.setColorFilter(ContextCompat.getColor(requireContext(), R.color.black))
+
+        binding.bottom.setOnClickListener{
+            calculateAge()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun calculateAge() {
+        val day = binding.editTextDay.text.toString().toIntOrNull()
+        val month = binding.editTextMonth.text.toString().toIntOrNull()
+        val year = binding.editTextYear.text.toString().toIntOrNull()
+
+        if (day == null || month == null || year == null) {
+            Toast.makeText(requireContext(), "Please enter a valid date", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val birthDate = LocalDate.of(year, month, day)
+        val currentDate = LocalDate.now()
+        val period = Period.between(birthDate, currentDate)
+
+        binding.numeroanys.text = period.years.toString()
+        binding.mesesnumero.text = period.months.toString()
+        binding.diasnumero.text = period.days.toString()
     }
 
 
